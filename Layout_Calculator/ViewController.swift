190f211d0647d42c749da807fd1d935e4ba8f194
Douglas Figueirôa <62218512+Douglas-euzinho@users.noreply.github.com>
2025-09-03 13:20:05 -0300

    Initial Commit

---
+++ Layout_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Layout_Calculator
+//
+//  Created by Douglas Figueirôa on 03/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
